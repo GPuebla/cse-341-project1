b53fbab6e9329e986e1d01dcb46dc0607326900c
GPuebla <gabrielpuebla16@gmail.com>
2024-10-03 08:49:56 -0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use("/", require("./routes"));
+const mongodb = require('/data/database');
 
 const port  = process.env.PORT || 3000;
-app.listen(port, ()=> {console.log('Running on port ${port}')})
+
+app.use("/", require("./routes"));
+
+mongodb.initDb((err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        app.listen(port, ()=> {console.log(`Database is listening and node is Running on port ${port}`)})
+    }
+})
+
