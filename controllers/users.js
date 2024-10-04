657867a19a738503e51df88c4a79d74443c37a49
GPuebla <gabrielpuebla16@gmail.com>
2024-10-04 08:28:11 -0300

    updating routes

--- controllers/users.js
+++ controllers/users.js
@@ -12,20 +12,12 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    try {
-        const userId = new ObjectId(req.params.id);
-        const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
-        const users = await result.toArray();
-        
-        if (users.length === 0) {
-            res.status(404).json({ message: "User not found" });
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(users[0]);
-        }
-    } catch (err) {
-        res.status(500).json({ message: "Error fetching user", error: err });
-    }
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('users').find({_id:userId});
+    result.toArray().then((users) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(users[0]);
+    });
 };
 
 module.exports = {
